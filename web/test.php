16f30530541e8cac89de161fe15f05b6785a3c3d
Biovor <duriez.ludovic@gmail.com>
2017-09-22 15:39:56 +0200

    modification 22/09

---
+++ web/test.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8" />
+    <meta name="description" content="Tout sur les apparitions muettes de
+la Banane Masquée dans le cinéma colombien des années 20 et 30." />
+    <link rel="stylesheet" href="style.css" />
+    <title>test</title>
+</head>
+
+<body>
+<div id="bloc_page">
+    <header>
+        <div id="titre_principal">
+            <div id="logo">
+                <img src="images/zozor_logo.png" alt="Logo de Zozor" />
+                <h1>Zozor</h1>
+            </div>
+            <h2>Carnets de voyage</h2>
+        </div>
+
+        <nav>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">CV</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div id="banniere_image">
+        <div id="banniere_description">
+            Retour sur mes vacances aux États-Unis...
+            <a href="#" class="bouton_rouge">Voir l'article <img src="images/flecheblanchedroite.png" alt="" /></a>
+        </div>
+    </div>
+
+
+    <footer>
+        <div id="tweet">
+            <h1>Mon dernier tweet</h1>
+    </footer>
+</div>
+</body>
+</html>
+
