226d2b607f570191ac418937cd79ef9bf8656a8b
Biovor <duriez.ludovic@gmail.com>
2017-10-02 16:21:38 +0200

    bug fix

--- src/AppBundle/Controller/Big5Controller.php
+++ src/AppBundle/Controller/Big5Controller.php
@@ -12,8 +12,10 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Big5;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\File\Stream;
 use Symfony\Component\HttpFoundation\Request;
 use GuzzleHttp\Client;
+use Spipu\Html2Pdf\Html2Pdf;
 
 class Big5Controller extends Controller
 {
@@ -74,7 +76,7 @@ class Big5Controller extends Controller
                 $big5->setDescription($json->archetype->description);
             }
             if (isset($json->pdfReport)) {
-                $big5->setPdfReport(base64_decode($json->pdfReport));
+                $big5->setPdfReport($json->pdfReport);
             }
 
             $em = $this->getDoctrine()->getManager();
@@ -85,5 +87,45 @@ class Big5Controller extends Controller
 
         return $this->render(
             'AppBundle:MonkeyTie:rep5.html.twig');
+    }
+
+    /**
+     * @Route("big5/response/pdf", name="big5Reppdf")
+     */
+    public function big5ReppdfAction(){
+
+        $em = $this->getDoctrine()->getManager();
+
+        $big5User = $em->getRepository('AppBundle:Big5')->findOneByuserId($this->getUser());
+        $pdf = base64_decode(utf8_encode($big5User->getPdfReport()));
+
+//        $file = '/web/big5/big5.pdf';
+//
+////        $current = file_get_contents($file);
+//
+//        $current = $pdf;
+//
+//        file_put_contents($file, $current);
+
+//        $file = 'testpdf.odt';
+//// Ouvre un fichier pour lire un contenu existant
+//        $current = file_get_contents($file);
+//// Ajoute une personne
+//        $current .= $pdf;
+//// Écrit le résultat dans le fichier
+//        file_put_contents($file, $current);
+//
+//       $fp= fopen('ppddff.pdf', 'w+');
+//        fwrite($fp, $pdf);
+//        fclose($fp);
+
+
+        $html2pdf = new Html2Pdf();
+        $html2pdf->writeHTML($pdf);
+        $html2pdf->output('big5.pdf');
+
+        return $this->render('AppBundle:MonkeyTie:rep5pdf.html.twig', array(
+            'pdf' => $pdf,
+        ));
     }
 }
