c0778d6da1189a8167573d822f9a64dbedc88e8c
Biovor <duriez.ludovic@gmail.com>
2017-10-26 17:55:50 +0200

    modif page tests

--- web/js/script.js
+++ web/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('.slider').slider({indicators:false, transition:550, interval:8750});
+    $('.slider').slider({indicators:false, transition:550, interval:8760});
     $('.slider').slider('next');
 });
 $(document).ready(function () {
