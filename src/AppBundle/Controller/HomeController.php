91a7766f676dcf692580c4975e5deea30c89ea6b
Biovor <duriez.ludovic@gmail.com>
2017-12-18 12:53:21 +0100

    test hook

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -107,8 +107,9 @@ class HomeController extends Controller
         if (!$cache->has('ocs')){
             $cache ->set('ocs', $request, 85000);
         }
+        $serv = explode("\" [\"",$cache->get('ocs')->server);
 
-        var_dump($cache->get('ocs')->server);
+        var_dump($serv);
         die();
         $secret =  $this->container->getParameter('secret_hook_cats');
         $webhookBody = $request->getContent();
