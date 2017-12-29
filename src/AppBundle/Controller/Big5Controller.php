a7e3f8c0510ca2e279c78a12e3d1a8135db6423c
Biovor <duriez.ludovic@gmail.com>
2017-12-29 12:39:51 +0100

    blabbla big5

--- src/AppBundle/Controller/Big5Controller.php
+++ src/AppBundle/Controller/Big5Controller.php
@@ -144,7 +144,7 @@ class Big5Controller extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $big5User = $em->getRepository('AppBundle:Big5')->findOneByuserId(3);
-        $user = $em->getRepository('UserBundle:User')->findOneById(3);
+        $user = $em->getRepository('UserBundle:User')->findById(3);
 
         $origin = 'Big5';
         $directory = 'big5/big5-'.$big5User->getId().'.pdf';
